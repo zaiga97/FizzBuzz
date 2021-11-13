52e358aa9a478ebf120e3e5105723262074e79a1
zaiga97 <zaiga97@gmail.com>
2021-11-13 11:56:31 +0100

    Extended version implementation

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,14 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
-        for (int i=1; i <= 100; i++){
-            if (i%3 == 0 && i%5 == 0) System.out.println("FizzBuzz");
+        for (int i=1; i <= 105; i++){
+            if (i%3 == 0 && i%5 == 0 && i%7 == 0) System.out.println("FizzBuzzBang");
+            else if (i%3 == 0 && i%7 == 0) System.out.println("FizzBang");
+            else if (i%5 == 0 && i%7 == 0) System.out.println("BuzzBang");
+            else if (i%3 == 0 && i%5 == 0) System.out.println("FizzBuzz");
             else if (i%3 == 0) System.out.println("Fizz");
             else if (i%5 == 0) System.out.println("Buzz");
+            else if (i%7 == 0) System.out.println("Bang");
             else System.out.println(i);
         }
     }
