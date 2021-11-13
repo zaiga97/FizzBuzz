75e22b53ada40526323eb55017e37dbce71b0ed0
zaiga97 <zaiga97@gmail.com>
2021-11-13 12:13:20 +0100

    A more elegant solution

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -4,14 +4,13 @@ public class FizzBuzz {
 
     public static void main(String... args) {
         for (int i=1; i <= 105; i++){
-            if (i%3 == 0 && i%5 == 0 && i%7 == 0) System.out.println("FizzBuzzBang");
-            else if (i%3 == 0 && i%7 == 0) System.out.println("FizzBang");
-            else if (i%5 == 0 && i%7 == 0) System.out.println("BuzzBang");
-            else if (i%3 == 0 && i%5 == 0) System.out.println("FizzBuzz");
-            else if (i%3 == 0) System.out.println("Fizz");
-            else if (i%5 == 0) System.out.println("Buzz");
-            else if (i%7 == 0) System.out.println("Bang");
-            else System.out.println(i);
+            String st = "";
+            if (i%3 == 0) st += "Fizz";
+            if (i%5 == 0) st += "Buzz";
+            if (i%7 == 0) st += "Bang";
+
+            if (st == "") System.out.println(i);
+            else System.out.println(st);
         }
     }
 
